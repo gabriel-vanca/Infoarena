2cbaf6b991e55bc81e0a361161a2b8da03e1a4e2
Gabriel Vanca <gabriel@vanca.co.uk>
2024-08-24 18:57:56 +0100

    env setup

---
+++ A+B (adunare)/adunare.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+int main() {
+    // Open the input file
+    std::ifstream inputFile("adunare.in");
+    if (!inputFile) {
+        std::cerr << "Failed to open input file." << std::endl;
+        return 1;
+    }
+
+    // Read the two numbers from the input file
+    int a, b;
+    inputFile >> a >> b;
+
+    // Close the input file
+    inputFile.close();
+
+    // Compute the sum
+    int sum = a + b;
+
+    // Open the output file
+    std::ofstream outputFile("adunare.out");
+    if (!outputFile) {
+        std::cerr << "Failed to open output file." << std::endl;
+        return 1;
+    }
+
+    if (outputFile.fail())
+    {
+        std::cout << "Failed to open output file.\n";
+    }
+
+    // Write the sum to the output file
+    outputFile << sum << std::endl;
+
+    // Close the output file
+    outputFile.close();
+
+    return 0;
+}
---
+++ Hello_World/hello_world.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    printf("Hello, from Hello_World!\n");
+
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        printf("%s ",word.c_str());
+        //  std::print(" ",word);
+        cout << word << " ";
+
+    }
+    cout << endl << endl;
+
+    return 0;
+}
